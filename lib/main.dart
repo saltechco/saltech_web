9b06ee66693d09f4e9af827e179557f89d70b23d
Mohammad Saleh Masoudi Moghadam <m.smasoudi72@gmail.com>
2023-09-10 18:19:41 +0330

    transfer commit

--- lib/main.dart
+++ lib/main.dart
@@ -241,8 +241,6 @@ class _DesktopEditionState extends State<DesktopEdition> {
     );
   }
 
-
-  // Test
   FutureBuilder<Comments> buildBody(BuildContext context) {
     final client =
         CommentsApi(Dio(BaseOptions(contentType: "application/json")));
